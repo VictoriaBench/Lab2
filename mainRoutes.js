5682dc8e8d0bbfdb8813798ce471229535fefbb1
VictoriaBench <victoria.bench@gmail.com>
2021-03-30 16:44:53 +0200

    test without web config file

--- mainRoutes.js
+++ mainRoutes.js
@@ -3,7 +3,7 @@ const express = require('express')
 const mainRouter = express.Router()
 
 mainRouter.get('/', function (req, res) {
-  res.send('hello world I\'m node! for real so serious....!!! do do dodo do dod od d')
+  res.send('hello world I\'m node! for real so serious....!!! do do dodo do dod od d hello again')
 })
 
 mainRouter.get('/about', function (req, res) {
